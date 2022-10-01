52471f63d18a98b3f591981d86936b16eec6d28d
Brian Baecher <brian.baecher@gmail.com>
2022-10-01 15:57:18 -0400

    date/image search working for now

--- app.js
+++ app.js
@@ -7,16 +7,34 @@ const dateInput = document.querySelector("#date");
 //array for tracking searches
 let dates  = []
 
-//function to add dates from HTML input
 const addDate = (e) => {
     e.preventDefault();
     console.log("add date ran");
+    //current date search entry as object
     let date = {
       id: Date.now(),
       content: dateInput.value,
     };
+    // push curr date obj to dates
     dates.push(date);
-    //form.reset();
+    form.reset();
+    fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=20rAsiKpg1mQjVMJKVdCDRa0GRWk1qapVLrf6SAH&date=${date.content}`
+    )
+    .then((response) => {
+        return response.json();
+    }).then((data) => image.src = data.url)
   };
 
 form.addEventListener("submit", addDate);
+
+function nasaTest() {
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=20rAsiKpg1mQjVMJKVdCDRa0GRWk1qapVLrf6SAH&date=2010-01-01"
+    )
+      .then((response) => {
+        return response.json();
+      })
+  
+      .then((data) => (image.src = data.url));
+  }
